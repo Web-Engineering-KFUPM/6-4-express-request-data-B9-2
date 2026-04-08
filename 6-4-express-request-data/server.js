cb7db5a3d485d6e2c3e9d19d4cc38e75d317ba8f
B9-2 <s202260760@kfupm.edu.sa>
2026-04-08 12:00:31 +0300

    6-4-express-request-data-B9-2

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -153,7 +153,7 @@ app.param("userId", (req, res, next, userId) => {
 
 // Route params: /users/:userId route
 app.get("/users/:userId", (req, res) => {
-   req.json({
+   res.json({
       ok: true,
       userId: req.userIdNum
    });
